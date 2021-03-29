0389354315f7d3fcbbdaf46f6ae0b2a58fa4c1f1
Felix <tinylambda@gmail.com>
2021-03-29 15:36:32 +0800

    add test for future stub in grpc client

--- src/main/java/com/example/grpc/GrpcClient.java
+++ src/main/java/com/example/grpc/GrpcClient.java
@@ -6,6 +6,7 @@ import java.util.concurrent.TimeUnit;
 import com.example.grpc.gencode.HelloRequest;
 import com.example.grpc.gencode.HelloResponse;
 import com.example.grpc.gencode.HelloServiceGrpc;
+import com.google.common.util.concurrent.ListenableFuture;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
@@ -16,13 +17,23 @@ public class GrpcClient {
         try {
             channel = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext().build();
             HelloServiceGrpc.HelloServiceBlockingStub stub = HelloServiceGrpc.newBlockingStub(channel);
-            HelloResponse response = stub.hello(HelloRequest.newBuilder().setFirstName("Pan").setLastName("Fei").build());
-            System.out.println(response.getGreeting());
+            HelloServiceGrpc.HelloServiceFutureStub stub2 = HelloServiceGrpc.newFutureStub(channel);
+
+            for (int i=0; i<100; i++) {
+                HelloResponse response = stub.hello(HelloRequest.newBuilder().setFirstName("Pan").setLastName("Fei").build());
+                System.out.println(response);
+            }
+            System.out.println("done 1");
+
+            for (int i=0; i<10; i++) {
+                ListenableFuture<HelloResponse> future = stub2.hello(HelloRequest.newBuilder().setFirstName("Pan").setLastName("Fei").build());
+                System.out.println(future.get());
+            }
+            System.out.println("done 2");
         } finally {
             if (channel != null) {
                 channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
             }
         }
-
     }
 }
