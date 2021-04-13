b6c6d2354157b5032a361563f4acd7bc31bcc16a
Felix <tinylambda@gmail.com>
2021-04-13 21:26:53 +0800

    use log instead of System.println

--- src/main/java/com/example/grpc/GrpcServer.java
+++ src/main/java/com/example/grpc/GrpcServer.java
@@ -5,14 +5,16 @@ import java.util.concurrent.Executors;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class GrpcServer {
     public static void main(String[] args) throws Exception {
         int port = 50051;
         final Server server = ServerBuilder.forPort(port)
                 .executor(Executors.newFixedThreadPool(8))
                 .addService(new HelloServiceImpl()).build().start();
-        System.out.println("server started, listening on " + port);
+        log.info("server started, listening on " + port);
         server.awaitTermination();
     }
 }
