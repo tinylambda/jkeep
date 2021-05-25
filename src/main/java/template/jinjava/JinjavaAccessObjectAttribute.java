3c9d6f408a04cd3b0e9b5528dfd7c54e1d49e7a0
Felix <tinylambda@gmail.com>
2021-05-25 14:44:15 +0800

    jinjava call object's method works

--- src/main/java/template/jinjava/JinjavaAccessObjectAttribute.java
+++ src/main/java/template/jinjava/JinjavaAccessObjectAttribute.java
@@ -12,16 +12,50 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class JinjavaAccessObjectAttribute {
+
+    static class Test {
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        private String name;
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+
+        private String value;
+
+        public Test(String name, String value) {
+            this.name = name;
+            this.value = value;
+        }
+    }
+
     public static void main(String[] args) {
         Jinjava jinjava = new Jinjava();
         Map<String, Object> context = Maps.newHashMap();
-        List<String> list = new ArrayList<>();
-        list.add("hi");
+        List<Test> list = new ArrayList<>();
+        list.add(new Test("name1", "value1"));
+        list.add(new Test("name2", "value2"));
 
         context.put("name", "felix");
         context.put("list", list);
 
-        String template = "The length of list is {{ list.size() }} {% if list.size() > 0 %} got list! {% else %} list is empty {% endif %}";
+        String template = "The length of list is {{ list.size() }} " +
+                "{% if list.size() > 0 %} " +
+                "{% for item in list %} {{ item.getName() }}='{{ item.getValue() }}' {% endfor %}" +
+                "{% else %} " +
+                "list is empty " +
+                "{% endif %}";
         String rendered = jinjava.render(template, context);
         log.info(rendered);
     }
