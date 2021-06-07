a6bbf28d3fd91217b7a0f0d1b06d182c9c717e34
Felix <tinylambda@gmail.com>
2021-06-07 18:13:52 +0800

    add existing key

--- src/main/java/collections/map/MapComputeIfAbsent.java
+++ src/main/java/collections/map/MapComputeIfAbsent.java
@@ -9,7 +9,10 @@ import lombok.extern.slf4j.Slf4j;
 public class MapComputeIfAbsent {
     public static void main(String[] args) {
         Map<String, Integer> map = new HashMap<>();
+        map.put("x", 2000);
         Integer integer = map.computeIfAbsent("key", k -> 1000);
+        log.info("the data is {}", integer);
+        integer = map.computeIfAbsent("x", k -> 9999);
         log.info("the data is {}", integer);
     }
 }
