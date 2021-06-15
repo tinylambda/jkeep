4998d3182795ae73e9e09e6abdfc7b29b0305b8e
Felix <tinylambda@gmail.com>
2021-06-15 09:46:45 +0800

    transform a stream to a map

---
+++ src/main/java/stream/StreamToMap.java
@@ -0,0 +1,33 @@
+package stream;
+
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.google.common.collect.Lists;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class StreamToMap {
+    @Data
+    @AllArgsConstructor
+    static class Point {
+        private String name;
+        private int x;
+        private int y;
+    }
+
+    public static void main(String[] args) {
+        List<Point> points = Lists.newArrayList(
+                new Point("p1", 10, 20),
+                new Point("p2", 1, 2),
+                new Point("p3", 11, 22)
+        );
+        Map<String, Point> pointMap = points.stream().collect(Collectors.toMap(Point::getName, point->point));
+        log.info("pointMap is {}", pointMap);
+    }
+}
