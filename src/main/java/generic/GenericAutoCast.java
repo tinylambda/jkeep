53e1042c07b4249c08e47f8ca8c271f3af10eb1b
Felix <tinylambda@gmail.com>
2021-06-25 09:57:19 +0800

    cast type a new way

---
+++ src/main/java/generic/GenericAutoCast.java
@@ -0,0 +1,32 @@
+package generic;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Maps.newHashMap;
+
+import java.util.List;
+import java.util.Map;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class GenericAutoCast {
+    private static final Map<String, Object> cache = newHashMap();
+    static {
+        cache.put("string", "hello");
+        cache.put("integer", 100);
+        cache.put("long", 100L);
+        cache.put("list", newArrayList());
+    }
+
+    public <T> T cacheGet(String key) {
+        return (T) cache.get(key);
+    }
+
+    public static void main(String[] args) {
+        GenericAutoCast genericAutoCast = new GenericAutoCast();
+        log.info("{}", genericAutoCast.<List<String>>cacheGet("list"));
+        log.info("{}", genericAutoCast.<Long>cacheGet("long"));
+        log.info("{}", genericAutoCast.<Integer>cacheGet("integer"));
+        log.info("{}", genericAutoCast.<String>cacheGet("string"));
+    }
+}
