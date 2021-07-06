0dd94567af80b43801d3e01d6e5e5263b59487d1
Felix <tinylambda@gmail.com>
2021-07-06 17:42:01 +0800

    more on thread pool exception

--- src/main/java/concurrent/executors/ConcurrentThreadPoolException.java
+++ src/main/java/concurrent/executors/ConcurrentThreadPoolException.java
@@ -2,7 +2,6 @@ package concurrent.executors;
 
 
 import java.util.Random;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -13,49 +12,46 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class ConcurrentThreadPoolException {
+    static Random random = new Random();
+
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         ExecutorService executorService = Executors.newFixedThreadPool(4);
-        for(int i=0; i<10; i++) {
-            executorService.submit(new Runnable() {
-                @Override
-                public void run() {
-                    Random random = new Random();
-                    int s = random.nextInt(3);
-                    log.info(Thread.currentThread().getName() + " sleeping " + s);
-                    try {
-                        TimeUnit.SECONDS.sleep(s);
-                        throw new RuntimeException("Exception");
-                    } catch (Exception e) {
-                        log.info("Current thread name is {}", Thread.currentThread().getName());
-                    }
-                }
-            });
-        }
-//        Future future = executorService.submit(new Callable<Boolean>() {
-//            @Override
-//            public Boolean call() throws Exception {
+
+//        for(int i=0; i<10; i++) {
+//            executorService.submit((Runnable) () -> {
 //                Random random = new Random();
-//                int s = random.nextInt(5);
-//                if (s % 2 == 0) {
-//                    log.info("EVEN !!!!!");
-//                    throw new RuntimeException("even number");
-//                } else {
-//                    return true;
+//                int s = random.nextInt(3);
+//                log.info(Thread.currentThread().getName() + " sleeping " + s);
+//                try {
+//                    TimeUnit.SECONDS.sleep(s);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
 //                }
+//                throw new RuntimeException("Exception");
+//            });
+//        }
+
+        Future<Boolean> future = executorService.submit(() -> {
+            int s = random.nextInt(5);
+            if (s % 2 == 0) {
+                log.info("EVEN !!!!!");
+                throw new RuntimeException("even number");
+            } else {
+                return true;
+            }
+        });
+
+//        Future future = executorService.submit(new Runnable() {
+//            @Override
+//            public void run() {
+//                log.info("go");
 //            }
 //        });
 //        log.info("result is {}", future.get());
 
-        Future future = executorService.submit(new Runnable() {
-            @Override
-            public void run() {
-                log.info("go");
-            }
-        });
-        log.info("result is {}", future.get());
-
         executorService.shutdown();
         executorService.awaitTermination(1, TimeUnit.DAYS);
+        log.info("result is {}", future.get());
         log.info("done");
     }
 }
