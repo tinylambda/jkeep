a3a5811aa2aa347291587e5fcff2ec9b242f5c4b
Felix <tinylambda@gmail.com>
2021-07-09 18:28:31 +0800

    cannot use a stream multiple times

---
+++ src/main/java/stream/StreamMultiTimesUsage.java
@@ -0,0 +1,23 @@
+package stream;
+
+import static java.util.stream.Collectors.toList;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.google.common.collect.Lists;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class StreamMultiTimesUsage {
+    public static void main(String[] args) {
+        List<String> list = Lists.newArrayList("a", "b", "c");
+        Stream<String> stringStream = list.stream();
+        List<String> x = stringStream.map(s -> s + ".").collect(toList());
+        List<String> y = stringStream.map(s -> s + "!").collect(toList());
+        log.info("{}", x);
+        log.info("{}", y);
+    }
+}
