3214a10ec7e84405e2c2128de666d12ab97bb0de
Felix <tinylambda@gmail.com>
2021-07-28 09:49:31 +0800

    more on default value

--- src/main/java/object/ObjectDefaultValue.java
+++ src/main/java/object/ObjectDefaultValue.java
@@ -5,11 +5,20 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class ObjectDefaultValue {
+    static class Test {}
+
     private int x;
     private float y;
     private double z;
     private boolean b;
+
     private String s;
+    private Integer xx;
+    private Float yy;
+    private Double zz;
+    private Boolean bb;
+
+    private Test tt;
 
     public static void main(String[] args) {
         ObjectDefaultValue defaultValue = new ObjectDefaultValue();
@@ -17,6 +26,12 @@ public class ObjectDefaultValue {
         log.info("{}", defaultValue.y);
         log.info("{}", defaultValue.z);
         log.info("{}", defaultValue.b);
+
         log.info("{}", defaultValue.s);
+        log.info("{}", defaultValue.xx);
+        log.info("{}", defaultValue.yy);
+        log.info("{}", defaultValue.zz);
+        log.info("{}", defaultValue.bb);
+        log.info("{}", defaultValue.tt);
     }
 }
