6470cf81d05d8e31613405b36f5ce6416eead395
Felix <tinylambda@gmail.com>
2021-09-22 17:59:11 +0800

    stream join

---
+++ src/main/java/stream/StreamJoining.java
@@ -0,0 +1,17 @@
+package stream;
+
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class StreamJoining {
+    public static void main(String[] args) {
+       String result = Stream.of("hello", "world", null)
+               .filter(Objects::nonNull)
+               .collect(Collectors.joining(";"));
+       log.info("{}", result);
+    }
+}
