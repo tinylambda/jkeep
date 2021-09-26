76523f11b913e1f3bcdb3f77194de4deb1909c43
Felix <tinylambda@gmail.com>
2021-09-26 16:00:04 +0800

    test kotlin interface

---
+++ src/main/java/kt/KtTODO.kt
@@ -0,0 +1,29 @@
+package kt
+
+/**
+ * @author panfei <panfei@kuaishou.com>
+ * Created on 2021-09-26
+ */
+
+interface I {
+    fun fun1(name: String)
+    fun fun2(name: String)
+}
+
+
+class A : I {
+    override fun fun1(name: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun fun2(name: String) {
+        println("your name is $name")
+    }
+}
+
+
+fun main() {
+    val a = A()
+    a.fun2("Felix")  // this is OK
+    a.fun1("Felix")  // this will trigger kotlin.NotImplementedError
+}
