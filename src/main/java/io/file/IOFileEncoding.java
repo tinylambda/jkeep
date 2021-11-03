f43a88283e2969b3e9501e3bf2796d634c1e57d1
Felix <tinylambda@gmail.com>
2021-11-03 11:57:43 +0800

    read from GBK file and write to UTF8 fuile

--- src/main/java/io/file/IOFileEncoding.java
+++ src/main/java/io/file/IOFileEncoding.java
@@ -1,7 +1,12 @@
 package io.file;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
 import lombok.extern.slf4j.Slf4j;
@@ -27,5 +32,24 @@ public class IOFileEncoding {
 
         log.info("{}", EncodingUtils.getEncode(utf8File.getAbsolutePath(), true));
         log.info("{}", EncodingUtils.getEncode(gbkFile.getAbsolutePath(), true));
+
+
+        File utf8FileTwo = File.createTempFile("helloUtf8Two", ".txt");
+        try (
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(gbkFile.getAbsolutePath()), "GBK"));
+                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(utf8FileTwo.getAbsolutePath()));
+        ) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                bufferedWriter.append(line);
+            }
+        }
+
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(utf8FileTwo.getAbsolutePath())))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                log.info("line: {}", line);
+            }
+        }
     }
 }
