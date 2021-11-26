91af81dc7fa5f8edcbbc1b42613b7d3507a46a14
Felix <tinylambda@gmail.com>
2021-11-26 18:43:28 +0800

    variable name replace number with _number

--- src/main/java/guava/GuavaUnderscore2Camel.java
+++ src/main/java/guava/GuavaUnderscore2Camel.java
@@ -1,5 +1,13 @@
 package guava;
 
+import static com.google.common.collect.Lists.newArrayList;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang3.StringUtils;
+
 import com.google.common.base.CaseFormat;
 
 import lombok.extern.slf4j.Slf4j;
@@ -13,5 +21,20 @@ public class GuavaUnderscore2Camel {
     public static void main(String[] args) {
         String s = "UPLOAD_SERVICE";
         log.info("{}", CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, s));
+
+        // replace number with _number
+        s = "orientation1";
+        Pattern pattern = Pattern.compile("(\\d+)");
+        Matcher matcher = pattern.matcher(s);
+        List<String> placeholders = newArrayList();
+        List<String> variableNames = newArrayList();
+        while (matcher.find()) {
+            placeholders.add(matcher.group(0));
+            variableNames.add(matcher.group(1));
+        }
+        for (int i = 0; i < placeholders.size(); i++) {
+            s = StringUtils.replaceOnce(s, placeholders.get(i), "_" + variableNames.get(i));
+        }
+        log.info("{}", s);
     }
 }
