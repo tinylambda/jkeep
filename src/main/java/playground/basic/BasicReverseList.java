9b053a4a03e48dbdf0af138aa48f73432cedcaa4
Felix <tinylambda@gmail.com>
2021-12-24 10:56:28 +0800

    reverse a list

---
+++ src/main/java/playground/basic/BasicReverseList.java
@@ -0,0 +1,86 @@
+package playground.basic;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author felix <tinylambda@gmail.com>
+ * Created on 2021-12-23
+ */
+@Slf4j
+public class BasicReverseList {
+    @AllArgsConstructor
+    @Data
+    static class Node {
+         private int value;
+         private Node next;
+    }
+
+    static Node reverse(Node head) {
+        Node currentNode = head.getNext();  // the node right after the head
+
+        Node newHead = new Node(-10, null);
+
+        Node toAttachNode = new Node(-20, null);
+        Node tmpNode;
+
+        while (true) {
+            tmpNode = currentNode.getNext();
+            currentNode.setNext(toAttachNode);
+            toAttachNode = currentNode;
+
+            // tmpNode reach the end
+            if (tmpNode.getNext() == null) {
+                break;
+            }
+            currentNode = tmpNode;
+        }
+
+        // attach the head to the new list
+        newHead.setNext(currentNode);
+        return newHead;
+    }
+
+    static Node findTailNode(Node head) {
+        if (head == null) {
+            return null;
+        }
+
+        Node currentNode = head;
+        while (currentNode.getNext() != null) {
+            currentNode = currentNode.getNext();
+        }
+        return currentNode;
+    }
+
+    static void printList(Node head) {
+        if (head == null) {
+            return;
+        }
+
+        Node currentNode = head;
+        log.info("HEAD: {}", currentNode.getValue());
+        currentNode = head.getNext();
+        while (currentNode.getNext() != null) {
+            log.info("{}", currentNode.getValue());
+            currentNode = currentNode.getNext();
+        }
+        log.info("TAIL: {}", currentNode.getValue());
+    }
+
+    public static void main(String[] args) {
+        Node tail = new Node(-2, null);
+        Node node1 = new Node(99, tail);
+        Node node2 = new Node(98, node1);
+        Node node3 = new Node(97, node2);
+        Node node4 = new Node(96, node3);
+        Node head = new Node(-1, node4);
+        printList(head);
+
+        log.info("\n\nthe reversed list: \n");
+
+        Node newHead = reverse(head);
+        printList(newHead);
+    }
+}
